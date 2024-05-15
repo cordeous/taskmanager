c1aa2cdc4bfceeaa4274ec56b0cab2dc4c9b7ac5
AJ <antojardon@gmail.com>
2024-05-15 07:29:11 -0400

    Update delete-list.php

--- delete-list.php
+++ delete-list.php
@@ -2,24 +2,33 @@
    
     include('config/constants.php');
     
+    // Verificar si se ha enviado 'list_id' a través de GET
     if(isset($_GET['list_id'])){
+        // Obtener el 'list_id' de GET
         $list_id = $_GET['list_id'];
+        // Conectar a la base de datos
         $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());    
-         $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+        // Seleccionar la base de datos
+        $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
         
+        // Consulta para eliminar la lista con el 'list_id' proporcionado
         $sql = "DELETE FROM tbl_lists WHERE list_id=$list_id";
         
+        // Ejecutar la consulta
         $res = mysqli_query($conn, $sql);
         
+        // Verificar si la consulta se ejecutó correctamente
         if($res==true)
         {
-            $_SESSION['delete'] = "List Deleted Successfully";
-            
+            // Establecer mensaje de sesión para indicar que la lista se eliminó correctamente
+            $_SESSION['delete'] = "Lista eliminada exitosamente";
+
             header('location:manage-list.php');
         }
         else
         {
-            $_SESSION['delete_fail'] = "Failed to Delete List.";
+            // Establecer mensaje de sesión en caso de que no se pueda eliminar la lista
+            $_SESSION['delete_fail'] = "Error al eliminar la lista.";
             header('location:manage-list.php');
         }
     }
@@ -27,9 +36,4 @@
     {
         header('location:manage-list.php');
     }
-    
-
-    
-    
-    
 ?>
