81b8fc11a91185dfda46fc77e296ccf03f674cfb
AJ <antojardon@gmail.com>
2024-05-17 07:54:49 -0400

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,10 +3,10 @@
 session_start();
 
 //Create Constants to save Database Credentials
-define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'S224DB_antoniojs');
+define('LOCALHOST', 'localhost'); // or IP
+define('DB_USERNAME', 'username'); //username
+define('DB_PASSWORD', 'password'); //password
+define('DB_NAME', 'database'); //database
 
 define('SITEURL', 'http://localhost/task-manager/');
 
